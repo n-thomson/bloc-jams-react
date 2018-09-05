6bccd37e1fa471754c40b3d8f9029f34e4b86b4c
Neethu Thomson <n***t@gmail.com>
2018-09-05 11:58:33 -0400

    added landing and library routes

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Landing extends Component{
+  render(){
+    return(
+      <div className = 'landing'>
+        <p>Landing page will go here</p>
+      </div>
+    )
+  }
+}
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Library extends Component{
+  render(){
+    return(
+      <div className = 'library'>
+        <p>Library page will go here</p>
+      </div>
+    )
+  }
+}
+
+export default Library;
