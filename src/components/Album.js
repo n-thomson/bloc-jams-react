b48ebc0c772ab6427fd3bacf46d10ed7fe5414d5
Neethu Thomson <n***t@gmail.com>
2018-09-05 15:51:07 -0400

    created album route and component

---
+++ src/components/Album.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Album extends Component{
+  render(){
+    return(
+      <div className = 'album'>
+        <p>Album page will go here</p>
+      </div>
+    )
+  }
+}
+
+export default Album;
