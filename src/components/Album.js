0897c66b041c71a0ad8ee934f05c910e8826dd69
Neethu Thomson <n***t@gmail.com>
2018-09-06 16:42:55 -0400

    linked library component to album

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 class Album extends Component{
   render(){
     return(
-      <div className = 'album'>
-        <p>Album page will go here</p>
-      </div>
+      <section className = 'album'>
+      {this.props.match.params.slug} Album will go here
+      </section>
     )
   }
 }
